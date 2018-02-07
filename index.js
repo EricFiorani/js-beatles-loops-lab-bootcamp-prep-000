2a98de4be6cc777df4bedeeff3d8cfad0bb59f28
Eric Fiorani <efiorani305@gmail.com>
2018-02-07 04:06:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(array){
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+  var facts = [];
   var moreFacts = [];
   var i = 0
   while (i < facts.length){
-    moreFacts.push(facts[i] + "!!!")
+    array.push(facts[i] + "!!!")
     i++;
   }
   return moreFacts;
